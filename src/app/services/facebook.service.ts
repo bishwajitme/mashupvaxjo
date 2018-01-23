18270d7c42ed8f3386000f50171cf39ccca41166
bishwajitme <iam@bishwajit.me>
2018-01-23 17:11:19 +0100

    Update facebook.service.ts

--- src/app/services/facebook.service.ts
+++ src/app/services/facebook.service.ts
@@ -19,7 +19,7 @@ export class FbService {
 
     getResults(keyword: string) {
 
-        let url = "https://graph.facebook.com/v2.11/search?type=place&q="+keyword+"&center=56.8770413,14.8092744&distance=1000&fields=name,checkins,picture,description,location,hours&access_token=2040144306206867|23e390ccd395736bfc12c2fd5cec9297";
+        let url = "https://graph.facebook.com/v2.11/search?type=place&q="+keyword+"&center=56.8770413,14.8092744&distance=1000&fields=name,checkins,picture,description,location,hours&access_token=Youraccesstoken";
 
         // API call
         return this.http.get(url);
